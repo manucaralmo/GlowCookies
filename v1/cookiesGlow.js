d71e63dacce5d4756b1271d489b77ec8591a53b1
Manuel Carrillo <manucaralmo@gmail.com>
2020-10-03 19:30:46 +0200

    cookies preselector

--- v1/cookiesGlow.js
+++ v1/cookiesGlow.js
@@ -5,8 +5,17 @@
   2020
 */
 
+// SELECTOR DE PREFERENCIAS
+var preBanner = document.createElement("div");
+preBanner.innerHTML = `<a href="#" id="prebanner" onClick="abrirSelector()" class="cookie-prebanner">
+                    <svg fill="currentColor" style="margin-right: 0.25em; margin-top: 0.15em; vertical-align: text-top;" height="1.05em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path d="M510.52 255.82c-69.97-.85-126.47-57.69-126.47-127.86-70.17 0-127-56.49-127.86-126.45-27.26-4.14-55.13.3-79.72 12.82l-69.13 35.22a132.221 132.221 0 0 0-57.79 57.81l-35.1 68.88a132.645 132.645 0 0 0-12.82 80.95l12.08 76.27a132.521 132.521 0 0 0 37.16 72.96l54.77 54.76a132.036 132.036 0 0 0 72.71 37.06l76.71 12.15c27.51 4.36 55.7-.11 80.53-12.76l69.13-35.21a132.273 132.273 0 0 0 57.79-57.81l35.1-68.88c12.56-24.64 17.01-52.58 12.91-79.91zM176 368c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm32-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm160 128c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"/>
+                    </svg>
+                    Cookies
+                </a>`;
+
 // Añadimos el HTML
-var cookies = document.createElement("cookieconsent");
+var cookies = document.createElement("div");
 cookies.innerHTML = `
 <div class="cookie-consent-banner">
     <div class="cookie-consent-banner__inner">
@@ -43,10 +52,11 @@ function activarSeguimiento(){
 var linkElement = document.createElement('link');
 linkElement.setAttribute('rel', 'stylesheet');
 linkElement.setAttribute('href', 'https://glowcookies.b-cdn.net/v1/cookiesGlow.min.css'); 
+document.body.appendChild(linkElement);
 
 // Nombre de las cookies
 var hostname = window.location.hostname;
-var cookieName = `cookies${hostname}`;
+var cookieName = `cookies${hostname}dE`;
 
 // Aceptar Cookies
 function aceptarCookies(){
@@ -55,6 +65,7 @@ function aceptarCookies(){
   var expires = "expires="+ d.toUTCString();
   document.cookie = `${cookieName}=aceptadas; ${expires}`;
   cookies.innerHTML = "";
+  preBanner.style.display === "none";
   activarSeguimiento();
 }
 
@@ -65,6 +76,13 @@ function rechazarCookies(){
   var expires = "expires="+ d.toUTCString();
   document.cookie = `${cookieName}=denegadas; ${expires}`;
   cookies.innerHTML = "";
+  preBanner.style.display === "block";
+}
+
+// abrir selector de cookies
+function abrirSelector(){
+  preBanner.style.display === "none";
+  document.body.appendChild(cookies);
 }
 
 // Verificar si existe la cookie
@@ -95,13 +113,13 @@ function rechazarCookies(){
     case "ok":
       console.log('Cookies: Aceptadas');
       activarSeguimiento();
+      document.body.appendChild(preBanner);
       break;
     case "no":
       console.log('Cookies: Denegadas');
-      // code block
+      document.body.appendChild(preBanner);
       break;
     default:
       console.log('Cookies: Sin escoger');
-      document.body.appendChild(linkElement);
       document.body.appendChild(cookies);
   }
